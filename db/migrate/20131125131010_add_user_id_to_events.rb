6bcc146bc4d82d896aa85cc550fc98ddd5936588
Yi Chin <wlq.yc1119@gmail.com>
2013-12-09 22:51:53 +0800

    delete remove column :events, :name

--- db/migrate/20131125131010_add_user_id_to_events.rb
+++ db/migrate/20131125131010_add_user_id_to_events.rb
@@ -2,7 +2,5 @@ class AddUserIdToEvents < ActiveRecord::Migration
   def change
   	add_column :events , :user_id, :integer
   	add_index :events, :user_id
-  	remove_column :events, :name
-
   end
 end
