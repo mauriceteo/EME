4caea636d7b4b8c414ae8d73970dc453bdd58fab
Yi Chin <wlq.yc1119@gmail.com>
2013-12-13 18:55:57 +0800

    delete unnessary pictures in assets, modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ EME::Application.routes.draw do
   devise_for :users
 
   resources :events
-  root to: 'events#index'
 
 match 'contact' => 'contact#new', :as => 'contact', :via => :get
 match 'contact' => 'contact#create', :as => 'contact', :via => :post
@@ -74,7 +73,7 @@ match 'contact' => 'contact#create', :as => 'contact', :via => :post
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
